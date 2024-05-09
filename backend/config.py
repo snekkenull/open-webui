d1b5533c8ca184703744fc232cd8348330fcb557
USAGI <124278082+snekkenull@users.noreply.github.com>
2024-05-09 16:55:46 +0800

    fix conflict

--- backend/config.py
+++ backend/config.py
@@ -71,10 +71,10 @@ except ImportError:
     log.warning("dotenv not installed, skipping...")
 
 WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
-#if WEBUI_NAME != "Open WebUI":
-#    WEBUI_NAME += " (Open WebUI)"
+if WEBUI_NAME != "Open WebUI":
+    WEBUI_NAME += " (Open WebUI)"
 
-WEBUI_FAVICON_URL = "/static/logo.png"
+WEBUI_FAVICON_URL = "https://openwebui.com/favicon.png"
 
 ####################################
 # ENV (dev,test,prod)
